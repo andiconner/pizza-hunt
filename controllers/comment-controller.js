adb6b2268633ec5537e9aa94fb47487d01c93639
Andi Conner <andiconner@icloud.com>
2022-02-17 11:17:18 -0500

    add reply

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,48 +1,79 @@
-const {Comment,Pizza} = require('../models');
+const { Comment, Pizza } = require('../models');
+
 const commentController = {
-    // add comment to pizza
-    addComment({ params, body }, res) {
-      console.log(body);
-      Comment.create(body)
-        .then(({ _id }) => {
-          return Pizza.findOneAndUpdate(
-            { _id: params.pizzaId },
-            { $push: { comments: _id } }, // we're using the $push method to add the comment's _id to the specific pizza we want to update. The $push method works just the same way that it works in JavaScript—it adds data to an array.
-            { new: true } // we're receiving back the updated pizza (the pizza with the new comment included)
-          );
-        })
-        .then(dbPizzaData => {
-          if (!dbPizzaData) {
-            res.status(404).json({ message: 'No pizza found with this id!' });
-            return;
-          }
-          res.json(dbPizzaData);
-        })
-        .catch(err => res.json(err));
-    },
   
-    // remove comment
-    removeComment({ params }, res) {
-      Comment.findOneAndDelete({ _id: params.commentId })
-        .then(deletedComment => {
-          if (!deletedComment) {
-            return res.status(404).json({ message: 'No comment with this id!' });
-          }
-          return Pizza.findOneAndUpdate(
-            { _id: params.pizzaId },
-            { $pull: { comments: params.commentId } },
-            { new: true }
-          );
-        })
-        .then(dbPizzaData => {
-          if (!dbPizzaData) {
-            res.status(404).json({ message: 'No pizza found with this id!' });
-            return;
-          }
-          res.json(dbPizzaData);
-        })
-        .catch(err => res.json(err));
-    }
-  };
+  // we're receiving back the updated pizza (the pizza with the new comment included)  // add comment to pizza
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body)
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // add reply to comment
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate({ _id: params.commentId }, { $push: { replies: body } }, { new: true })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // remove comment
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({ _id: params.commentId })
+      .then(deletedComment => {
+        if (!deletedComment) {
+          return res.status(404).json({ message: 'No comment with this id!' });
+        }
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+  // remove reply
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
+  }
+};
 
 module.exports = commentController;
+
+ 
+ 
+          
+    
+    
